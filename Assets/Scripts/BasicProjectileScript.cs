82131ae07c474a423610692be2cae6d590317246
Nicholasdputra <Nicholasdputra@gmail.com>
2025-07-25 15:47:17 +0700

    Anims
    Anims Done (for now)

--- Assets/Scripts/BasicProjectileScript.cs
+++ Assets/Scripts/BasicProjectileScript.cs
@@ -4,6 +4,7 @@ public class BasicProjectileScript : MonoBehaviour
 {
     public int damage;
     public int speed;
+    public int lane;
 
     // Update is called once per frame
     void Update()
@@ -14,7 +15,7 @@ public class BasicProjectileScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy") && other.GetComponent<Enemy>().row + 1 == lane)
         {
             AudioManagerScript.Instance.PlaySFX(AudioManagerScript.Instance.turretProjectileHitSFXClip);
             Debug.Log("Hit an enemy!");
